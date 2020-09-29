85cf28bba9b39728479d96f1934a08e94f770b09
Stephane Fontaine <bsfc1685@gmail.com>
2020-09-29 01:01:20 -0400

    0.2.33: version bump

--- src/codybot.c
+++ src/codybot.c
@@ -12,7 +12,7 @@
 
 #include "codybot.h"
 
-const char *codybot_version_string = "0.2.32";
+const char *codybot_version_string = "0.2.33";
 
 static const struct option long_options[] = {
 	{"help", no_argument, NULL, 'h'},

610f23a7fe046f6d7b5df23a721b5292560545b1
Stephane Fontaine <bsfc1685@gmail.com>
2020-11-01 02:57:12 -0500

    0.3.4: version update

--- src/codybot.c
+++ src/codybot.c
@@ -12,7 +12,7 @@
 
 #include "codybot.h"
 
-const char *codybot_version_string = "0.3.3";
+const char *codybot_version_string = "0.3.4";
 
 static const struct option long_options[] = {
 	{"help", no_argument, NULL, 'h'},

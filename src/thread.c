e6743d0a4c88c5663feb672609706b5f167a4c87
Stephane Fontaine <bsfc1685@gmail.com>
2020-11-12 23:20:25 -0500

    0.3.4: fix tcc refusing local functions

--- src/thread.c
+++ src/thread.c
@@ -128,6 +128,19 @@ void ThreadRXStart(void) {
 	pthread_attr_destroy(&attr);
 }
 
+int CheckCTCPTime(void) {
+	t0 = time(NULL);
+	if (strcmp(raw.nick, ctcp_prev_nick) == 0 && ctcp_prev_time <= t0-5) {
+		ctcp_prev_time = t0;
+		return 0;
+	}
+	else {
+		ctcp_prev_time = t0;
+		sprintf(ctcp_prev_nick, "%s", raw.nick);
+		return 1;
+	}
+}
+
 void *ThreadRXFunc(void *argp) {
 	while (!endmainloop) {
 		memset(buffer_rx, 0, 4096);
@@ -161,19 +174,6 @@ void *ThreadRXFunc(void *argp) {
 
 		RawLineParse(&raw, buffer_rx);
 		RawGetTarget(&raw);
-
-		int CheckCTCPTime(void) {
-			t0 = time(NULL);
-			if (strcmp(raw.nick, ctcp_prev_nick) == 0 && ctcp_prev_time <= t0-5) {
-				ctcp_prev_time = t0;
-				return 0;
-			}
-			else {
-				ctcp_prev_time = t0;
-				sprintf(ctcp_prev_nick, "%s", raw.nick);
-				return 1;
-			}
-		}
 
 		// Respond to few CTCP requests
 		if (strcmp(raw.text, "\001CLIENTINFO\x01") == 0) {

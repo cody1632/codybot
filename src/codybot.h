c6c5d754e5b5fa9d4aa44a2bd5cb27f66e7d6851
Stephane Fontaine <bsfc1685@gmail.com>
2020-11-27 18:40:21 -0500

    1.0.3: added important comments

--- src/codybot.h
+++ src/codybot.h
@@ -4,27 +4,32 @@
 #include <sys/time.h>
 #include <openssl/ssl.h>
 
+// To control which compiler !cc use. (via var cc_compiler)
 #define CC_COMPILER_GCC 1
 #define CC_COMPILER_TCC 2
 
 // Globals from codybot.c
 extern const char *codybot_version_string;
-extern int debug, socket_fd, ret, endmainloop, cc_disabled, sh_disabled, 
-	sh_locked, wttr_disabled, cmd_timeout, use_ssl, cc_compiler;
-extern unsigned long long fortune_total;
-extern struct timeval tv0, tv_start;
+extern int debug, socket_fd, ret, endmainloop, cc_compiler, cc_disabled, sh_disabled, 
+	sh_locked, wttr_disabled, cmd_timeout, use_ssl;
+extern unsigned long long fortune_total; // set with $srcdir/stats content
+extern struct timeval tv0, tv_start; // 0 sort of means now and "start" is program start time
 extern struct tm *tm0;
-extern time_t t0, ctcp_prev_time;
+extern time_t t0, ctcp_prev_time; // ctcp antiflood considerations
 extern char ctcp_prev_nick[128];
-extern char *log_filename;
+extern char *log_filename; // $srcdir/codybot.log if not set, see main()
+// 'buffer' is always for immediate use. 'rx' is for received raw data. 'cmd' is
+// mostly used in system() calls. 'log' is used for data to $srcdir/codybot.log
 extern char *buffer, *buffer_rx, *buffer_cmd, *buffer_log;
-extern char trigger_char;
-extern char *current_channel;
-extern char *nick;
-extern char *full_user_name;
+extern char trigger_char; // character which is prefixed to commands in IRC chat readout
+extern char *current_channel; // used as changeable default channel for console 
+                              // 'msg/fortune' cmd
+extern char *nick; // running bot's IRC nick
+extern char *full_user_name; // name displayed in IRC /whois responses
 extern char *hostname;
-extern char *target;
-extern char *colors[];
+extern char *target; // this one should be worked upon a little bit :P
+                     // used to differentiate channel-wide vs private messages
+extern char *colors[]; // IRC color codes used in '!colorize/!rainbow'
 
 // Globals from server.c
 extern unsigned int server_port, local_port;
@@ -52,20 +57,20 @@ extern struct raw_line raw;
 
 // from admin.c
 struct Admin {
-	struct Admin *prev, *next;
+	struct Admin *prev, *next; // used to navigate the list in a loop
 	char *nick, *host;
 };
 
 struct AdminList {
-	unsigned int total_admins;
-	struct Admin *first_admin, *last_admin;
+	unsigned int total_admins; // simple (unused yet???) counter
+	struct Admin *first_admin, *last_admin; // loop startpoints
 };
 extern struct AdminList admin_list;
 
 void AddAdmin(char *newnick, char *host);
 void DestroyAdminList(void);
 char *EnumerateAdmins(void);
-int IsAdmin(char *newnick, char *host);
+int IsAdmin(char *newnick, char *host); // used to check who can run specific commands
 void ParseAdminFile(void);
 
 // from cmd-*.c
